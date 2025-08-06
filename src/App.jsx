453ec248a01ab14dfe8e3ecd8d4b0b2e612559c2
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-08-06 16:14:12 +0200

    Tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import ProductList from "./components/ProductList";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <h1 className="text-2xl font-bold p-4">Platzi Fake Store</h1>
+    <div>
+      <h1>Platzi Fake Store</h1>
       <ProductList />
     </div>
   );
