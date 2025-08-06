af33cdfd3a0a428b80972886e3a775eedb7167f3
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-08-06 16:40:44 +0200

    Tailwind style for products

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -15,9 +15,13 @@ export default function ProductList() {
   if (!products.length) return <p>Loading...</p>;
 
   return (
-    <div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 justify-center">
       {products.map((product) => (
-        <div key={product.id}>
+        <div
+          key={product.id}
+          className="border-5 border-gray-500/20 rounded-lg p-4 cursor-pointer hover:scale-105 transition-transform duration-200"
+        >
+          <p>id: {product.id}</p>
           <img src={product.images[0]} alt={product.title} />
           <h2>{product.title}</h2>
           <p className="text-sm text-gray-600 mb-4">
