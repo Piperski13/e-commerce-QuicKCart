002496fbee50bb530c277280ac9d6220635500df
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-08-11 14:45:17 +0200

    Added Link component for img and title for a cart page item

--- src/components/CartPageItem.jsx
+++ src/components/CartPageItem.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 const CartPageItem = (props) => {
   console.log("props cartPageItem: ", props.product);
   return (
     <div className="space-y-6">
       <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6">
         <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
-          <a href="#" className="shrink-0 md:order-1">
+          <Link
+            to={`/product/${props.product.id}`}
+            href="#"
+            className="shrink-0 md:order-1"
+          >
             <img
               className="h-20 w-20 dark:hidden"
               src={props.product.image}
@@ -15,7 +20,7 @@ const CartPageItem = (props) => {
               src={props.product.image}
               alt={props.product.title}
             />
-          </a>
+          </Link>
 
           <label className="sr-only">Choose quantity:</label>
           <div className="flex items-center justify-between md:order-3 md:justify-end">
@@ -66,12 +71,13 @@ const CartPageItem = (props) => {
           </div>
 
           <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
-            <a
+            <Link
+              to={`/product/${props.product.id}`}
               href="#"
               className="text-base font-medium text-gray-900 hover:underline dark:text-white"
             >
               {props.product.title}
-            </a>
+            </Link>
 
             <div className="flex items-center gap-4">
               <button
