3304f84e6e3d461a99ea1652413ae5b9ded94943
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-08-18 14:18:45 +0200

    Cart style change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,17 +24,13 @@ const Header = (props) => {
       <Navbar />
       <Link to="/cart">
         <div className="relative text-zinc-50 hover:text-zinc-500">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1"
-            className="w-8 h-8"
-          >
-            <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-1.6 8h13.2M7 13L5.4 5M17 17a2 2 0 11-4 0 2 2 0 014 0zm-6 0a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
-          <p className="absolute -bottom-3 -right-1 font-bold text-red-400">
+          <img
+            width="48"
+            height="48"
+            src="src/assets/cart.png"
+            alt="shopping-cart--v1"
+          />
+          <p className="absolute -bottom-3 -right-2 font-bold text-red-500 text-xl text-shadow-md text-shadow-zinc-800">
             {quantity}
           </p>
         </div>
