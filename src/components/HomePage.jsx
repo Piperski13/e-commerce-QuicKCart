2b28b99a1459a45b298b16073ea2bc77a87f446c
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-08-20 14:49:01 +0200

    Mobile Header page responsive

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div className="flex items-center justify-center flex-col h-screen">
-      <h1 className="text-6xl font-bold">
+      <h1 className="lg:text-6xl text-5xl text-center font-bold">
         Shop quickly with{" "}
         <span className="bg-gradient-to-r from-purple-400 to-[#101828] bg-clip-text text-transparent">
           QuicKCart.
         </span>
       </h1>
-      <p className="text-zinc-400 mb-2 mt-10 w-200 text-center">
+      <p className="text-zinc-400 mb-2 mt-10 lg:w-200 w-100 text-center">
         Your one-stop shop for fast, easy, and reliable online shopping. Get
         what you need in just a few clicks.
       </p>
