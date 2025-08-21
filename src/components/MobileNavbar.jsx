aaf77cc39c246a4a2387cc50497ce9576c8b1be6
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-08-21 15:17:37 +0200

    Small changes for LCP Optimisation

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -15,7 +15,7 @@ const MobileNavbar = () => {
       <div
         className={`fixed top-0 right-0 h-full w-2/3 bg-gray-800 text-white transform ${
           open ? "translate-x-0" : "translate-x-full"
-        } transition-transform duration-300 ease-in-out z-1`}
+        } transition-transform duration-300 ease-in-out z-2`}
       >
         <div className="flex flex-col p-6">
           <Link to="/home" onClick={() => setOpen(false)} className="mb-2">
